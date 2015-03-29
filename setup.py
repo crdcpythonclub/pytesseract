8d510d7409ecc5de6b6aa29695666f0ba8d5bb18
cartman.djw@gmail.com <cartman.djw@gmail.com>
2015-03-29 14:31:58 +0800

    Fix bugs in setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@ from setuptools import setup
 
 
 longDesc = ""
-if os.path.exits("README.md"):
+if os.path.exists("README.md"):
 	longDesc = open("README.md").read().strip()
 
 setup(
     name = "pytesseract",
     version = "0.1.6",
-    author = "Samuel Hoffstaetter",
+    author = "Samuel Hoffstaetter - Forked by Python Club",
     author_email="",
-    maintainer = "Matthias Lee",
-    maintainer_email = "pytesseract@madmaze.net",
+    maintainer = "cartman",
+    maintainer_email = "cartman.djw@gmail.com",
     description = ("Python-tesseract is a python wrapper for google's Tesseract-OCR"),
     long_description = longDesc,
     license = "GPLv3",
     keywords = "python-tesseract OCR Python",
-    url = "https://github.com/madmaze/python-tesseract",
+    url = "https://github.com/crdcpythonclub/pytesseract",
     packages=['pytesseract'],
     package_dir={'pytesseract': 'src'},
     package_data = {'pytesseract': ['*.png','*.jpg']},
